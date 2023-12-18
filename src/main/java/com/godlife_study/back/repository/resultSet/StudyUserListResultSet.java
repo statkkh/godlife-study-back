482b2a3838fe8104d42873ca17f0343a09e23070
kkh <qr21@naver.com>
2023-12-18 17:15:24 +0900

    FIX : StudyUserListResultSet 타입 수정
    String getStudyNumber() → int getStudyNumber()

--- src/main/java/com/godlife_study/back/repository/resultSet/StudyUserListResultSet.java
+++ src/main/java/com/godlife_study/back/repository/resultSet/StudyUserListResultSet.java
@@ -2,6 +2,6 @@ package com.godlife_study.back.repository.resultSet;
 
 public interface StudyUserListResultSet {
     String getUserEmail();        // 특정 유저를 받아와하니까 추가
-    String getStudyNumber();    
+    int getStudyNumber();    
     String getStudyGrade();
 }
