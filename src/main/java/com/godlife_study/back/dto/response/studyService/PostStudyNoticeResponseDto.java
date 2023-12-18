541335b71c4e60b83130bd9b541755490bb9d082
khkim <stakkh4@gmail.com>
2023-12-18 11:20:39 +0900

    FEAT PostStudyNoticeResponseDto- 서비스에서 성공할 때  와 실패할 때 나오는 경우의수및 db에 값을 저장하기 까지의 로직

---
+++ src/main/java/com/godlife_study/back/dto/response/studyService/PostStudyNoticeResponseDto.java
@@ -0,0 +1,35 @@
+package com.godlife_study.back.dto.response.studyService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.godlife_study.back.dto.response.ResponseCode;
+import com.godlife_study.back.dto.response.ResponseDto;
+import com.godlife_study.back.dto.response.ResponseMessage;
+
+public class PostStudyNoticeResponseDto extends  ResponseDto{
+
+    private PostStudyNoticeResponseDto(String code, String message){
+        super(code, message);
+    }
+
+        public static ResponseEntity<PostStudyNoticeResponseDto> success(){
+        PostStudyNoticeResponseDto  result = new PostStudyNoticeResponseDto(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistUser(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXIST_USER, ResponseMessage.NOT_EXIST_USER);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+    
+    public static ResponseEntity<ResponseDto> notExistStudy(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXIST_STUDY, ResponseMessage.NOT_EXIST_STUDY);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> noPermission(){
+        ResponseDto result = new ResponseDto(ResponseCode.NO_PERMISSION, ResponseMessage.NO_PERMISSION);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+}
