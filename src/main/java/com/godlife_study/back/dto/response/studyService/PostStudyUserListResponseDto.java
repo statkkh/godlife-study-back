93cf1ef435f9952946240959f0ddf1d107735c66
kkh <qr21@naver.com>
2023-12-18 16:21:28 +0900

    Feat response  dto - PostStudyUserListResponseDto 생성 및  스터디 유저  작성시 실패와 성공에 대한 응답 코드와 메시지 생성및 성공시 DB 스터디 유저 리스트 테이블에 추가

---
+++ src/main/java/com/godlife_study/back/dto/response/studyService/PostStudyUserListResponseDto.java
@@ -0,0 +1,33 @@
+package com.godlife_study.back.dto.response.studyService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.godlife_study.back.dto.response.ResponseCode;
+import com.godlife_study.back.dto.response.ResponseDto;
+import com.godlife_study.back.dto.response.ResponseMessage;
+
+import lombok.Getter;
+
+@Getter
+public class PostStudyUserListResponseDto extends ResponseDto {
+    
+    private PostStudyUserListResponseDto(String code, String message){
+        super(code, message);
+    }
+
+    public static ResponseEntity<PostStudyUserListResponseDto> success() {
+        PostStudyUserListResponseDto  result = new PostStudyUserListResponseDto(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistUser() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXIST_USER, ResponseMessage.NOT_EXIST_USER);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistStudy() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXIST_STUDY, ResponseMessage.NOT_EXIST_STUDY);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+}
